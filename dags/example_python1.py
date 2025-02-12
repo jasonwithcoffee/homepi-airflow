dcf8d0627064db0a1b8ed568822e4dd13ec72aa7
jasonwithcoffee <54336962+jasonwithcoffee@users.noreply.github.com>
2025-02-12 10:05:33 -0800

    formatting with black

--- dags/example_python1.py
+++ dags/example_python1.py
@@ -9,25 +9,24 @@ from airflow.operators.bash import BashOperator
 
 with DAG(
     dag_id="example_python1",
-    start_date=datetime(2025,1,1),
+    start_date=datetime(2025, 1, 1),
     schedule_interval=timedelta(minutes=15),
     schedule=None,
     catchup=False,
-    default_args={"retries": 1}
+    default_args={"retries": 1},
 ):
-    def print_hello():
-        print("Hello world")
 
-    task1 = PythonOperator(
-        task_id="task1",
-        python_callable=print_hello
+    def get_datetime_now(ti):
+
+        # get time now and pass to xcom
+        now = datetime.now()
+        now_str = now.isoformat()
+
+        ti.xcom_push(key="datetime_now", value=now_str)
+
+    task1_get_time = PythonOperator(
+        task_id="task1_get_time", python_callable=get_datetime_now, provide_context=True
     )
-
-    task2 = BashOperator(
-        task_id="task2",
-        bash_command="echo 'Hello world'"
-    )
-
 
     def http_request(url):
         response = requests.get(url)
@@ -40,26 +39,28 @@ with DAG(
             print(f"Failed to retrieve data. Status code: {response.status_code}")
 
             # Raise an error to terminate the DAG
-            raise ValueError(f"Failed to retrieve data. Status code: {response.status_code}")
-    
+            raise ValueError(
+                f"Failed to retrieve data. Status code: {response.status_code}"
+            )
 
-    def get_data():
+    def get_data(ti):
         url = "https://meowfacts.herokuapp.com/"
         response = http_request(url)
         data = {}
-        data['data'] = response['data'][0]
+        data["data"] = response["data"][0]
         # Get current date and time
-        now = datetime.now()
-        data['timestamp'] = now.strftime("%Y-%m-%d %H:%M:%S")
+        # now = datetime.now()
+        datetime_str = ti.xcom_pull(task_ids="task1_get_time", key="datetime_now")
+        now = datetime.fromisoformat(datetime_str)
+        data["timestamp"] = now.strftime("%Y-%m-%d %H:%M:%S")
 
         print(data)
         return data
 
-    def get_filename():
+    def get_filename(ti):
         # Define target directory
         target_directory = "/opt/airflow/data/staging/"
         # expanded_directory = os.path.expanduser(target_directory)
-        
 
         # Create the directory if it doesn't exist
         os.makedirs(target_directory, exist_ok=True)
@@ -68,7 +69,8 @@ with DAG(
         print(contents)
 
         # Get current date and time
-        now = datetime.now()
+        datetime_str = ti.xcom_pull(task_ids="task1_get_time", key="datetime_now")
+        now = datetime.fromisoformat(datetime_str)
 
         # Format date and time as a string
         filename = now.strftime("file_%Y%m%d_%H%M.json")
@@ -77,39 +79,19 @@ with DAG(
         # Join the directory and filename
         filepath = target_directory + filename
         return filepath
-    
-    def get_data_and_save():
-        data = get_data()
-        filepath = get_filename()
+
+    def get_data_and_save(ti):
+        data = get_data(ti)
+        filepath = get_filename(ti)
 
         with open(filepath, "w") as file:
             json.dump(data, file, indent=4)
         print(f"Data saved to {filepath}")
 
-    task3 = PythonOperator(
-        task_id="task3",
-        python_callable=get_data_and_save
+    task2_api_call = PythonOperator(
+        task_id="task2_api_call",
+        python_callable=get_data_and_save,
+        provide_context=True,
     )
 
-
-
-
-    task1 >> task2 >> task3
-
-    '''
-        def print_hello():
-            print("Hello world")
-
-        task1 = PythonOperator(
-            task_id="task1",
-            python_callable=print_hello
-        )
-
-        task2 = BashOperator(
-            task_id="task2",
-            bash_command="echo 'Hello world'"
-        )
-
-        task1 >> task2
-
-    '''
+    task1_get_time >> task2_api_call
